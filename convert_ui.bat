d735b0654bf78213514ce7bd399804eb4e2e5a13
Steven <steven@windesktop.com>
2022-01-26 00:39:36 -0500

    moved convert_ui.bat to UI\ directory

--- convert_ui.bat
+++
@@ -1,7 +0,0 @@
-:: Convert all .ui files into python scripts
-@echo OFF
-echo Converting .ui files...
-pyuic5 UI\mainwindow.ui > UI\ui_mainwindow.py
-echo Compiling .qrc file...
-pyrcc5 resources.qrc -o Sources\resources.py
-echo Done
