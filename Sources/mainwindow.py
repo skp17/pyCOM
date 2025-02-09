3f379ecacb2b29dfcea49d5906a6e076d8b9fc0e
steven <steven@windesktop.com>
2025-02-08 20:10:53 -0500

    Program displays message if no COM are found

--- Sources/mainwindow.py
+++ Sources/mainwindow.py
@@ -6,7 +6,7 @@ import sys
 
 # Third-party modules
 from PyQt5 import QtCore, QtGui, QtSerialPort, uic
-from PyQt5.QtWidgets import QActionGroup, QApplication, QHeaderView, QMainWindow, QMessageBox
+from PyQt5.QtWidgets import QActionGroup, QApplication, QHeaderView, QMainWindow, QMessageBox, QLabel
 
 # Homemade modules
 import breeze_resources  # used for dark theme
@@ -43,19 +43,30 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
-        self._ports = []
+        self._ports = []  # Todo: no need for this variable
         self.tableModel = PortTableModel()
         self.setWindowIcon(QtGui.QIcon(":/icons/serial_port.ico"))
 
         self.tableView.setModel(self.tableModel)  # Set model to table view
         self.tableView.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
 
+        # Group the theme buttons
         self.groupTheme = QActionGroup(self.menuSettings)
         self.groupTheme.addAction(self.actionLight)
         self.groupTheme.addAction(self.actionDark)
         self.groupTheme.setExclusive(True)
         self.groupTheme.triggered.connect(self.change_theme)
 
+        # Create overlay label to display when no COM ports are found
+        self.empty_label = QLabel("No COM ports found", self.tableView)
+        self.empty_label.setAlignment(QtCore.Qt.AlignCenter)
+        self.empty_label.setStyleSheet("font-size: 14px; color: gray;")
+        self.empty_label.setVisible(False)
+
+        # Resize label when table resizes
+        self.tableView.viewport().installEventFilter(self)
+
+        # Connect signals
         self.tableView.clicked.connect(self.display_port_info)
         self.actionAbout.triggered.connect(self.show_about_dialog)
         self.actionRefresh.triggered.connect(self.display_ports)
@@ -66,16 +77,32 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         if event.type() == QtCore.QEvent.ActivationChange and self.isActiveWindow():
             self.display_ports()
 
+    def eventFilter(self, obj: QtCore.QObject, event: QtCore.QEvent):
+        """
+        Update label position when viewport resizes
+        """
+        if obj == self.tableView.viewport() and event.type() == event.Resize:
+            self.adjust_empty_label()
+        return super().eventFilter(obj, event)
+
     def change_theme(self, action):
         if action == self.actionLight:
             toggle_stylesheet(":/light.qss")
         elif action == self.actionDark:
             toggle_stylesheet(":/dark/stylesheet.qss")
 
+    def adjust_empty_label(self):
+        if self.tableModel.ports:
+            self.empty_label.setVisible(False)
+        else:
+            self.empty_label.setGeometry(self.tableView.viewport().rect())  # Make label cover the viewport
+            self.empty_label.setVisible(True)
+
     def display_ports(self):
         self._ports = [port for port in QtSerialPort.QSerialPortInfo.availablePorts()]  # Run inside separate thread
         self.tableModel.ports = self._ports
         self.tableModel.layoutChanged.emit()
+        self.adjust_empty_label()
 
     def display_port_info(self, index):
         port = self.tableModel.ports[index.row()]
